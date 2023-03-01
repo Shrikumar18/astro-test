bcda749ce60dd90c663effc9b90a352b9c46aa74
Shrikumar18 <shrikumar.d17@gmail.com>
2023-03-01 15:17:50 +0530

    Reverts SSR Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: cloudflare({ mode: "directory" }),
-  build: {
-    staticHeaders: {
-      '**/*.js': [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=300',
-        },
-      ],
-      '**/*.css': [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=300',
-        },
-      ],
-      '**/*.png': [
-        {
-          key: 'Cache-Control',
-          value: 'public, max-age=300',
-        },
-      ],
-    },
-  },
+  integrations: [react()]
 });
